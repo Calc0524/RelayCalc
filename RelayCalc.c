330383b1ea222854b30bb199fbb6d0f48efe5edd
roian6 <roian6@naver.com>
2020-05-24 20:25:00 +0900

    빼기 기능 추가, 오류 수정
    더하기 기능 짠사람 반성하세요ㅠㅠ

--- RelayCalc.c
+++ RelayCalc.c
@@ -3,10 +3,14 @@
 int main(){
 	int a, b;
 	int cal;
-	scanf("%d",&a);
-	scanf("%c",&cal);
-	scanf("%d",&b);
+	
+	printf("계산할 수식을 입력해주세요 (ex. 1+2)\n");
+	scanf("%d%c%d",&a, &cal, &b);
 	if(cal == '+'){
 		printf("%d + %d = %d",a,b,a+b);
 	}
+	else if(cal == '-'){
+		printf("%d - %d = %d",a,b,a-b);
+	}
+	//:carrot:
 }
